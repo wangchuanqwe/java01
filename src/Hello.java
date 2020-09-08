d5719740906fb426425ae3cb999c0f4d6d4a3e6f
wangchuanqwe <1362912374@qq.com>
2020-09-08 16:06:02 +0800

    Initial commi

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * @作者 王川
+ * @创建日期 2020/9/3
+ */
+public class Hello {
+
+    public static void main(String[] args){
+    System.out.println("hello，欢迎你！！！");
+
+    ArrayList list = new ArrayList<>();
+        }
+/**/
+        public  void methed(){
+
+        }
+}
